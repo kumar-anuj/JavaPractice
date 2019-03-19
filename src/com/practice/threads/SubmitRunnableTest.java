80e1a0a66feba9db335c3d38088b78f2162878cb
Anuj Kumar <87.anuj.kumar@gmail.com>
2019-03-19 19:32:55 +0530

    thread practice

---
+++ src/com/practice/threads/SubmitRunnableTest.java
@@ -0,0 +1,17 @@
+package com.practice.threads;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class SubmitRunnableTest {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService es = Executors.newSingleThreadExecutor();
+		Future<?> f = es.submit(() -> System.out.println("Runnable Test"));
+		Thread.sleep(500);
+		System.out.println("Output==>"+f.get());
+	}
+
+}
