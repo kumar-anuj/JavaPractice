32acd069bf7b40088a693c3633950afef0ae3393
Anuj Kumar <87.anuj.kumar@gmail.com>
2019-03-19 17:09:09 +0530

    corrected ternary condition

--- src/com/practice/datastructures/testpractice/FinalListTest.java
+++ src/com/practice/datastructures/testpractice/FinalListTest.java
@@ -8,7 +8,7 @@ public class FinalListTest {
 	public static void main(String[] args) {
 		int x = 5;
 		
-		System.out.println("Outut is :" + ((x>5) ? 3 : 2));
+		System.out.println("Outut is :" + ((x>5) ? 3.1 : 2));
 		
 		final List<String> lst = new ArrayList<>();
 		lst.add("one");
