2f1c0be807bb0af1f927ea0b9ba1c278e3466be8
Anuj Kumar <87.anuj.kumar@gmail.com>
2019-03-30 19:40:18 +0530

    print a map sorted on the basis of values

--- src/com/practice/java8/streams/reduction/StreamReductionExample2.java
+++ src/com/practice/java8/streams/reduction/StreamReductionExample2.java
@@ -1,8 +1,6 @@
 package com.practice.java8.streams.reduction;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,15 +9,33 @@ public class StreamReductionExample2 {
     public static void main(String[] args) {
         List<String> lst = createStringList();
 
-        Map<String, Long> str = lst.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        System.out.println(str);
+        Map<String, Long> strMap = lst.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println(strMap);
+        Collection<Long> values = strMap.values();
+        // Collection of long to Collection of Integer
+        Collection<Integer> intValues = values.stream().mapToInt(t -> t.intValue()).boxed().collect(Collectors.toList());
+        // Reverse sort
+        intValues = intValues.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        Set<String> mapKeys = strMap.keySet();
+        Map<String, Long> newMap = new LinkedHashMap<>();
+
+        //for each value find the matching key and add to new map
+        intValues.forEach( v -> mapKeys.forEach( k ->
+        {
+            if(strMap.get(k).intValue() == v)
+                newMap.put(k,v.longValue());
+        }));
+
+        System.out.println(newMap);
+
     }
 
     public static List<String> createStringList() {
         List<String> lst = new ArrayList<>();
-        lst.add("ONE");lst.add("TWO");
+        lst.add("ONE");lst.add("TWO");lst.add("ABC");lst.add("ONE");
         lst.add("TWO");lst.add("ONE");lst.add("ONE");
-        lst.add("FOUR");lst.add("FOUR");
+        lst.add("FOUR");lst.add("FOUR");lst.add("FOUR");
 
         return lst;
     }
